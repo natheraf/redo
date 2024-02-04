e2487d29d2d063e1162de72241fdb739f9689e74
jeffreyz69 <jeffreyz1234@gmail.com>
2024-02-03 21:32:50 -0500

    setup

---
+++ server/index.js
@@ -0,0 +1,5 @@
+//connecting express to react app in the client folder
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
