db1c0f3760be2b8f52c3b04a2c1f9ce3bb5fcea4
jeffreyz69 <jeffreyz1234@gmail.com>
2024-02-03 21:54:40 -0500

    connected node to express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 //connecting express to react app in the client folder
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("../client/build"));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Express Server on port ${port}`));
