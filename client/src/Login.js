e6882f75b88a0fef856bb89f8752ff51231cacf3
jeffreyz69 <jeffreyz1234@gmail.com>
2024-02-07 12:58:54 -0500

    simple text fields added

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,10 +6,15 @@ import { Link } from 'react-router-dom';
 //When they sign up then, store user info into local storage
 export const Login = () => {
     return (
-        <div className="Login" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <h1>
+        <div className="Login" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ textAlign: 'center' }}>
                 Login
             </h1>
+            
+            <input type="Username" style={{ marginBottom: '10px' }}/>
+
+            <input type="password" style={{ marginBottom: '10px' }}/>
+
         </div>
     );
 }
