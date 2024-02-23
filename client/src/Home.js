20c83512ab4ca8aad29dc0706b3254a1e365b770
jeffreyz69 <jeffreyz1234@gmail.com>
2024-02-23 02:16:51 -0500

    updated requirements on what todo in Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,9 @@
 //upon sucessful login, the user should be redirected to the home page.
-//temp: after the redirect, the user should see this page with some placeholder text
 //the user should also see a logout button which will redirect the user back to the landing page
+//The user should see an option to create a new to do list
+//The user should see a list of to do items that they have created
+//The user should be able to complete/delete a to do items
+//The user should be able to edit a to do item
 
 import React from 'react';
 
@@ -9,10 +12,11 @@ export const Home = () => {
         <div className="Home" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', marginTop: '-20vh' }}>
             <header className="Home-header">
                 <h1>Welcome to the Home Page</h1>
-                <p>
-                    This is a placeholder for the home page. 
-                </p>
+                
+
+
                 <a href="/">Logout</a>
+                
             </header>
         </div>
     );
